a97edeb21440d078506b50ae4ac3d041477afdd5
Michael Pohl <contact@michaelpohl.de>
2021-11-22 19:45:47 +0100

    Build basic dumpsys method that works

--- service/src/main/java/com/michaelpohl/service/MonitoringService.kt
+++ service/src/main/java/com/michaelpohl/service/MonitoringService.kt
@@ -1,11 +1,20 @@
 package com.michaelpohl.service
 
+import android.annotation.SuppressLint
 import android.app.*
+import android.content.Context
 import android.content.Intent
 import android.media.session.MediaSession
 import android.os.*
-import androidx.appcompat.app.AppCompatActivity
+import android.telephony.CellInfo
+import android.telephony.TelephonyManager
+import android.telephony.TelephonyManager.CellInfoCallback
+import androidx.annotation.RequiresApi
 import timber.log.Timber
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
 import java.util.*
 
 class MonitoringService : Service() {
@@ -14,26 +23,89 @@ class MonitoringService : Service() {
     private val notificationHandler = NotificationHandler()
     private lateinit var session: MediaSession
 
+    private lateinit var telephonyManager: TelephonyManager
+
     private lateinit var notificationManager: NotificationManager
     private lateinit var serviceHandler: Handler
 
     var activityClass: Class<out Activity>? = null
-    var serviceState = ServiceState.STOPPED
+    var serviceState = com.michaelpohl.service.ServiceState.STOPPED
     fun start() {
-        if (serviceState != ServiceState.RUNNING) startService(Intent(applicationContext, MonitoringService::class.java))
-        serviceState = ServiceState.RUNNING
+        if (serviceState != com.michaelpohl.service.ServiceState.RUNNING) startService(
+            Intent(
+                applicationContext,
+                MonitoringService::class.java
+            )
+        )
+        serviceState = com.michaelpohl.service.ServiceState.RUNNING
     }
 
     override fun onCreate() {
         Timber.d("Service created")
         super.onCreate()
 //        setupThread()
+        telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Timber.d("Service started")
         handleNotificationStopClicked(intent)
+//        CoroutineScope(Dispatchers.Main).launch {
+        testCellTowerInfo()
+//        }
         return START_NOT_STICKY
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCellTowerInfo() {
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    @SuppressLint("MissingPermission")
+    private fun testCellTowerInfo() {
+        Timber.d("TestCellTowerInfo")
+
+        try {
+            val arrayCommand = arrayOf("su", "-c", "dumpsys", "telephony.registry", "|", "grep", "\"mCi=\"")
+
+            val process = Runtime.getRuntime().exec(arrayCommand)
+
+            val processOutput = BufferedReader(InputStreamReader(process.inputStream)).readText()
+            val processError = BufferedReader(InputStreamReader(process.errorStream)).readText()
+
+            Timber.d("output: $processOutput")
+            if (processError.isNotEmpty()) Timber.d("error: $processError")
+        } catch (e: IOException) {
+            Timber.e(e)
+        }
+    }
+
+    fun runAsRoot(commands: List<String>) {
+        val p = Runtime.getRuntime().exec("su");
+        val os = DataOutputStream(p.outputStream);
+        val bufferedReader = BufferedReader(InputStreamReader(p.inputStream))
+        val bufferedReader2 = BufferedReader(InputStreamReader(p.errorStream))
+
+        val string = bufferedReader.readText()
+
+        commands.forEach {
+            os.writeBytes(it + "\n");
+        }
+        os.writeBytes("exit\n");
+        Timber.d("output: ${string}")
+        Timber.d("output: ${bufferedReader2.readText()}")
+        os.flush();
+    }
+
+    private val callback = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        object : CellInfoCallback() {
+            override fun onCellInfo(cellInfo: MutableList<CellInfo>) {
+                Timber.d("CellInfo")
+                Timber.d("$cellInfo")
+            }
+        }
+    } else {
+        TODO("VERSION.SDK_INT < Q")
     }
 
     private fun setupNotification() {
@@ -84,7 +156,7 @@ class MonitoringService : Service() {
 
     private fun stop() {
         Timber.d("Service stopped")
-        serviceState = ServiceState.STOPPED
+        serviceState = com.michaelpohl.service.ServiceState.STOPPED
         stopSelf()
     }
 
